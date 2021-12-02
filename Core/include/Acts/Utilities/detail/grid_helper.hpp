0b31b077011e2016e7cce30a49614cc2344e3279
scott snyder <sss@karma>
2021-12-02 16:23:56 -0500

    Add a couple missing const qualifiers.
    Fixes in passing warnings from clang12 in c++20 mode about comparision
    operator ambiguities.

--- Core/include/Acts/Utilities/detail/grid_helper.hpp
+++ Core/include/Acts/Utilities/detail/grid_helper.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2017-2018 CERN for the benefit of the Acts project
+// Copyright (C) 2017-2023 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -95,7 +95,7 @@ class GlobalNeighborHoodIndices {
       return *this;
     }
 
-    bool operator!=(const iterator& it) { return !(*this == it); }
+    bool operator!=(const iterator& it) const { return !(*this == it); }
 
     bool isEqual(const iterator& b) const {
       if (b.m_parent == nullptr) {
