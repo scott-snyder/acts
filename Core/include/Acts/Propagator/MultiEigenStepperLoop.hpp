7676560c70bb9592b292a88e8020245b2248ca5e
scott snyder <sss@karma>
2023-11-16 11:50:51 -0500

    Refresh of gcc14

--- Core/include/Acts/Propagator/MultiEigenStepperLoop.hpp
+++ Core/include/Acts/Propagator/MultiEigenStepperLoop.hpp
@@ -1,6 +1,6 @@
 // This file is part of the Acts project.
 //
-// Copyright (C) 2021 CERN for the benefit of the Acts project
+// Copyright (C) 2021-2023 CERN for the benefit of the Acts project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -493,12 +493,6 @@ class MultiEigenStepperLoop
   /// proxy internally holding a reference
   auto componentIterable(State& state) const {
     struct Iterator {
-      using difference_type = std::ptrdiff_t;
-      using value_type = ComponentProxy;
-      using reference = ComponentProxy;
-      using pointer = void;
-      using iterator_category = std::forward_iterator_tag;
-
       typename decltype(state.components)::iterator it;
       const State& s;
 
@@ -511,8 +505,6 @@ class MultiEigenStepperLoop
     };
 
     struct Iterable {
-      using iterator = Iterator;
-
       State& s;
 
       // clang-format off
@@ -530,12 +522,6 @@ class MultiEigenStepperLoop
   /// proxy internally holding a reference
   auto constComponentIterable(const State& state) const {
     struct ConstIterator {
-      using difference_type = std::ptrdiff_t;
-      using value_type = ConstComponentProxy;
-      using reference = ConstComponentProxy;
-      using pointer = void;
-      using iterator_category = std::forward_iterator_tag;
-
       typename decltype(state.components)::const_iterator it;
       const State& s;
 
@@ -548,7 +534,6 @@ class MultiEigenStepperLoop
     };
 
     struct Iterable {
-      using iterator = ConstIterator;
       const State& s;
 
       // clang-format off
