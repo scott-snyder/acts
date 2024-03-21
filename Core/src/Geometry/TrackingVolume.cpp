88b3350ebf9ec7c8f1340df066c53b4ad988c2fe
scott snyder <snyder@bnl.gov>
2024-03-21 19:01:19 +0100

    flatten

--- Core/src/Geometry/TrackingVolume.cpp
+++ Core/src/Geometry/TrackingVolume.cpp
@@ -1,6 +1,6 @@
 // This file is part of the ACTS project.
 //
-// Copyright (C) 2016 CERN for the benefit of the ACTS project
+// Copyright (C) 2016-2024 CERN for the benefit of the ACTS project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -426,6 +426,14 @@ TrackingVolume::compatibleBoundaries(const GeometryContext& gctx,
   return targets;
 }
 
+#if defined(FLATTEN) && defined(__GNUC__)
+// We compile this function with optimization, even in debug builds; otherwise,
+// the heavy use of Eigen makes it too slow.  However, from here we may call
+// to out-of-line Eigen code that is linked from other DSOs; in that case,
+// it would not be optimized.  Avoid this by forcing all Eigen code
+// to be inlined here if possible.
+[[gnu::flatten]]
+#endif
 boost::container::small_vector<NavigationTarget, 10>
 TrackingVolume::compatibleLayers(
     const GeometryContext& gctx, const Vector3& position,
