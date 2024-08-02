043a6b90e3a4e82033dcdd60a70d48d49aedf8d1
scott snyder <sss@karma>
2024-08-02 15:53:09 -0400

    Fix clang warnings: Binding references to temporaries in range-for.

--- Core/src/Seeding2/GraphBasedTrackSeeder.cpp
+++ Core/src/Seeding2/GraphBasedTrackSeeder.cpp
@@ -177,7 +177,7 @@ std::vector<std::vector<GbtsNode>> GraphBasedTrackSeeder::createNodes(
   std::vector<bool> pixelLayers{};
   pixelLayers.reserve(maxLayers);
 
-  for (const auto& sp : spacePoints) {
+  for (const auto sp : spacePoints) {
     // for every sp in container,
     // add its variables to nodeStorage organised by layer
     const std::uint16_t layer = sp.extra(layerColumn);
