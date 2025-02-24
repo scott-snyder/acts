f3b00622cc5e38044ce68551765da3c8c3ffabf0
sss <sss@karma>
2025-02-24 14:13:25 -0500

    Unused variable.

--- Fatras/src/Digitization/PlanarSurfaceMask.cpp
+++ Fatras/src/Digitization/PlanarSurfaceMask.cpp
@@ -1,6 +1,6 @@
 // This file is part of the ACTS project.
 //
-// Copyright (C) 2016 CERN for the benefit of the ACTS project
+// Copyright (C) 2016, 2025 CERN for the benefit of the ACTS project
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -80,7 +80,6 @@ Acts::Result<ActsFatras::PlanarSurfaceMask::Segment2D>
 ActsFatras::PlanarSurfaceMask::apply(const Acts::Surface& surface,
                                      const Segment2D& segment) const {
   auto surfaceType = surface.type();
-  Segment2D clipped(segment);
 
   // Plane surface section -------------------
   if (surfaceType == Acts::Surface::Plane ||
