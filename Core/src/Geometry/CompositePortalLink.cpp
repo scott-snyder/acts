bc5416fce70a1c54266638d6a3eb3c1729a44f87
sss <sss@karma>
2025-07-23 13:35:59 -0400

    Refresh of warn3

--- Core/src/Geometry/CompositePortalLink.cpp
+++ Core/src/Geometry/CompositePortalLink.cpp
@@ -251,7 +251,7 @@ std::unique_ptr<GridPortalLink> CompositePortalLink::makeGrid(
 
     auto gridPortalLink =
         GridPortalLink::make(m_surface, m_direction, std::move(axis));
-    for (const auto& [i, child] : enumerate(trivialLinks)) {
+    for (const auto [i, child] : enumerate(trivialLinks)) {
       gridPortalLink->grid().atLocalBins({i + 1}) = &child.volume();
     }
 
@@ -292,7 +292,7 @@ std::unique_ptr<GridPortalLink> CompositePortalLink::makeGrid(
     Axis axis{AxisBound, edges};
 
     auto grid = GridPortalLink::make(m_surface, m_direction, std::move(axis));
-    for (const auto& [i, child] : enumerate(trivialLinks)) {
+    for (const auto [i, child] : enumerate(trivialLinks)) {
       grid->grid().atLocalBins({i + 1}) = &child.volume();
     }
 
@@ -341,7 +341,7 @@ std::unique_ptr<GridPortalLink> CompositePortalLink::makeGrid(
     Axis axis{AxisBound, edges};
 
     auto grid = GridPortalLink::make(m_surface, m_direction, std::move(axis));
-    for (const auto& [i, child] : enumerate(trivialLinks)) {
+    for (const auto [i, child] : enumerate(trivialLinks)) {
       grid->grid().atLocalBins({i + 1}) = &child.volume();
     }
 
