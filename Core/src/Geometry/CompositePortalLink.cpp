da3689ccdd4dfbd5ebb65d42ba8bb36fb8507e5b
sss <sss@karma>
2025-07-23 13:35:54 -0400

    Hiding virtual functions from base class.

--- Core/src/Geometry/CompositePortalLink.cpp
+++ Core/src/Geometry/CompositePortalLink.cpp
@@ -254,7 +254,7 @@ std::unique_ptr<GridPortalLink> CompositePortalLink::makeGrid(
 
     auto gridPortalLink =
         GridPortalLink::make(m_surface, m_direction, std::move(axis));
-    for (const auto& [i, child] : enumerate(trivialLinks)) {
+    for (const auto [i, child] : enumerate(trivialLinks)) {
       gridPortalLink->grid().atLocalBins({i + 1}) = &child.volume();
     }
 
@@ -295,7 +295,7 @@ std::unique_ptr<GridPortalLink> CompositePortalLink::makeGrid(
     Axis axis{AxisBound, edges};
 
     auto grid = GridPortalLink::make(m_surface, m_direction, std::move(axis));
-    for (const auto& [i, child] : enumerate(trivialLinks)) {
+    for (const auto [i, child] : enumerate(trivialLinks)) {
       grid->grid().atLocalBins({i + 1}) = &child.volume();
     }
 
@@ -345,7 +345,7 @@ std::unique_ptr<GridPortalLink> CompositePortalLink::makeGrid(
     Axis axis{AxisBound, edges};
 
     auto grid = GridPortalLink::make(m_surface, m_direction, std::move(axis));
-    for (const auto& [i, child] : enumerate(trivialLinks)) {
+    for (const auto [i, child] : enumerate(trivialLinks)) {
       grid->grid().atLocalBins({i + 1}) = &child.volume();
     }
 
