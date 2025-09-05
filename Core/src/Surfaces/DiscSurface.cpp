a05713249bdf449903e5dca60d4ae405b274ec93
scott snyder <snyder@bnl.gov>
2025-09-05 22:15:35 +0200

    Refresh of flatten2

--- Core/src/Surfaces/DiscSurface.cpp
+++ Core/src/Surfaces/DiscSurface.cpp
@@ -95,6 +95,14 @@ Vector3 DiscSurface::localToGlobal(const GeometryContext& gctx,
   return transform(gctx) * loc3Dframe;
 }
 
+#if defined(FLATTEN) && defined(__GNUC__)
+// We compile this function with optimization, even in debug builds; otherwise,
+// the heavy use of Eigen makes it too slow.  However, from here we may call
+// to out-of-line Eigen code that is linked from other DSOs; in that case,
+// it would not be optimized.  Avoid this by forcing all Eigen code
+// to be inlined here if possible.
+[[gnu::flatten]]
+#endif
 Result<Vector2> DiscSurface::globalToLocal(const GeometryContext& gctx,
                                            const Vector3& position,
                                            double tolerance) const {
@@ -149,6 +157,14 @@ const SurfaceBounds& DiscSurface::bounds() const {
   return s_noBounds;
 }
 
+#if defined(FLATTEN) && defined(__GNUC__)
+// We compile this function with optimization, even in debug builds; otherwise,
+// the heavy use of Eigen makes it too slow.  However, from here we may call
+// to out-of-line Eigen code that is linked from other DSOs; in that case,
+// it would not be optimized.  Avoid this by forcing all Eigen code
+// to be inlined here if possible.
+[[gnu::flatten]]
+#endif
 Polyhedron DiscSurface::polyhedronRepresentation(
     const GeometryContext& gctx, unsigned int quarterSegments) const {
   // Prepare vertices and faces
@@ -231,6 +247,14 @@ BoundToFreeMatrix DiscSurface::boundToFreeJacobian(
   return jacToGlobal;
 }
 
+#if defined(FLATTEN) && defined(__GNUC__)
+// We compile this function with optimization, even in debug builds; otherwise,
+// the heavy use of Eigen makes it too slow.  However, from here we may call
+// to out-of-line Eigen code that is linked from other DSOs; in that case,
+// it would not be optimized.  Avoid this by forcing all Eigen code
+// to be inlined here if possible.
+[[gnu::flatten]]
+#endif
 FreeToBoundMatrix DiscSurface::freeToBoundJacobian(
     const GeometryContext& gctx, const Vector3& position,
     const Vector3& direction) const {
