963228cde1c42766f4d98ef94fb23718636d4ec3
sss <sss@karma>
2025-10-07 13:18:17 -0400

    Refresh of warn3

--- Core/src/Seeding2/CylindricalSpacePointGrid2.cpp
+++ Core/src/Seeding2/CylindricalSpacePointGrid2.cpp
@@ -182,7 +182,7 @@ void CylindricalSpacePointGrid2::extend(
   ACTS_VERBOSE("Inserting " << spacePoints.size()
                             << " space points to the grid");
 
-  for (const ConstSpacePointProxy2& sp : spacePoints) {
+  for (const ConstSpacePointProxy2 sp : spacePoints) {
     insert(sp);
   }
 }
