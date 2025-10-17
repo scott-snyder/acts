4f0e74b7bf4fc76aed823b088aa15354ca8bbe08
sss <sss@karma>
2025-10-17 13:19:47 -0400

    Need to include SurfaceArray.h to use with unique_ptr.
    Fixes compilation issues seen with clang.

--- Plugins/DD4hep/include/ActsPlugins/DD4hep/DD4hepFieldAdapter.hpp
+++ Plugins/DD4hep/include/ActsPlugins/DD4hep/DD4hepFieldAdapter.hpp
@@ -24,6 +24,7 @@ class DD4hepFieldAdapter : public Acts::MagneticFieldProvider {
 
  public:
   explicit DD4hepFieldAdapter(dd4hep::OverlayedField field);
+  ~DD4hepFieldAdapter();
 
   Acts::MagneticFieldProvider::Cache makeCache(
       const Acts::MagneticFieldContext& mctx) const override;
