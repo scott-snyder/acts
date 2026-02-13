bf635390b2d8bb5b26b855964dc6f9399eba2bf8
scott snyder <snyder@bnl.gov>
2026-02-13 13:32:15 +0100

    Refresh of flatten

--- Core/src/TrackFitting/GainMatrixSmoother.cpp
+++ Core/src/TrackFitting/GainMatrixSmoother.cpp
@@ -15,6 +15,14 @@
 
 namespace Acts {
 
+#if defined(FLATTEN) && defined(__GNUC__)
+// We compile this function with optimization, even in debug builds; otherwise,
+// the heavy use of Eigen makes it too slow.  However, from here we may call
+// to out-of-line Eigen code that is linked from other DSOs; in that case,
+// it would not be optimized.  Avoid this by forcing all Eigen code
+// to be inlined here if possible.
+[[gnu::flatten]]
+#endif
 Result<void> GainMatrixSmoother::calculate(AnyMutableTrackStateProxy ts,
                                            AnyConstTrackStateProxy prev_ts,
                                            const Logger& logger) const {
