f5d5a0922274e5d43c5aa5d64e1e6bcab359d50e
sss <sss@karma>
2026-06-12 12:40:12 -0400

    More warning fixes.

--- Plugins/Gnn/src/Tensor.cpp
+++ Plugins/Gnn/src/Tensor.cpp
@@ -84,7 +84,7 @@ Tensor<T> cudaSelectCols(const Tensor<T> &tensor, const Tensor<bool> &mask,
 
 }  // namespace detail
 
-void sigmoid(Tensor<float> &tensor, std::optional<cudaStream_t> stream) {
+void sigmoid(Tensor<float> &tensor, [[maybe_unused]] std::optional<cudaStream_t> stream) {
   if (tensor.device().type == Device::Type::eCUDA) {
 #ifdef ACTS_GNN_WITH_CUDA
     return ActsPlugins::detail::cudaSigmoid(tensor, stream.value());
